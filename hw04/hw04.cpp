ed971e0679682d61a4d785618c6025a6c629b2af
allenivan@gmail.com <allenivan@gmail.com>
2017-04-29 00:17:51 +0800

    commit

--- hw04/hw04.cpp
+++ hw04/hw04.cpp
@@ -1,4 +1,111 @@
 #include <iostream>
 #include <vector>
+#include <utility>
+#include <unordered_map>
+#include <cstdio>
+#include <cstdlib>
 using namespace std;
-int main(){}
+
+class MaxHeap{
+    public:
+        MaxHeap(){
+            pair<int,long long> node (-1,0);
+            tree.push_back(node);
+            iterator.push_back(-1);
+        }
+        ~MaxHeap();
+        bool empty(){return tree.size()==1;}
+        int size(){ return tree.size();}
+        void insert(int id, long long value);
+        void swapNode(int parent,int kid){
+            int tmp = iterator[kid];
+            iterator[kid] = iterator[parent];
+            iterator[parent] = tmp;
+        }
+        void print(){
+            for(int i = 1; i < iterator.size();i++) cout << tree[iterator[i]].first << " ";
+        }
+        void update(long long add, int id){
+            tree[iterator[pos[id]]].second+=add;
+            int iter = pos[id];
+            long long value = tree[iterator[pos[id]]].second;
+            upHeap(iter,id,value,0);
+        }
+        void upHeap(int iter, int id, long long value, int new_id = 1 ){
+            while(true){
+                if (iter==1) {
+                    pos[id] = iter;
+                    break;
+                }
+                int parent_pos = iter/2;
+                int parent_id = tree[iterator[parent_pos]].first;
+                long long parent_value = tree[iterator[parent_pos]].second;
+                if(parent_value < value || (parent_value == value && new_id == 1)){
+                    swapNode(parent_pos,iter);
+                    pos[parent_id] = iter;
+                    iter = parent_pos;
+                }
+                else{
+                    pos[id] = iter; 
+                    break;
+                }
+            }
+        }
+        void printRoot(long long deduct){
+            int root = iterator[1];
+            int id = tree[root].first;
+            long long value = tree[root].second - deduct;
+            cout << "id: " << id << ", value: " << value << "\n";
+        }
+    private:
+        vector< pair<int, long long> > tree;
+        unordered_map<int,int> pos;
+        vector<int> iterator;;
+};
+
+void MaxHeap::insert(int id, long long value){
+    pair<int, long long> node(id,value);
+    tree.push_back(node);
+    int current_pos = tree.size()-1;
+    iterator.push_back(current_pos);
+    int iter = iterator.size()-1;
+    upHeap(iter,id,value);
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    int action_number;
+    long long insert_count=0, deducted, tmp;
+    scanf("%d%lld", &action_number,&deducted);
+    vector<long long> data;
+    while(scanf("%lld", &tmp) != EOF) data.push_back(tmp);
+    MaxHeap* tree = new MaxHeap;
+    for(int i = 0; i < data.size(); i++){
+        switch(data[i]){
+            case 1:{
+                insert_count++;
+                int id = data[i+1];
+                long long value = data[i+2]+insert_count*deducted;
+                tree->insert(id, value);
+                i=i+2;
+                break;
+            }
+            case 2:{
+                long long add = data[i+1]; 
+                int n = data[i+2];
+                for(int j = 0; j < n; j++) {
+                    int current_id = data[i+3+j];
+                    tree->update(add,current_id);
+                }
+                i = i + 2 + n; 
+                break;
+            }
+            case 3:
+                tree->printRoot(insert_count*deducted);
+                break;
+            default:
+                break;
+        }
+    }
+    return 0;
+}
